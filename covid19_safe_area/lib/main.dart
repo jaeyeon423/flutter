296dd097c09761e382a4bb876abde5ec86409e1e
jaeyeon423 <jaeyeon423@gmail.com>
2021-09-11 14:40:00 +0900

    [covid 19] xml

--- covid19_safe_area/lib/main.dart
+++ covid19_safe_area/lib/main.dart
@@ -14,6 +14,7 @@ class ExampleWidget extends StatefulWidget {
 
 class ExampleState extends State<ExampleWidget> {
   MapDataSource? _dataSource;
+  late double headerTopZone = 10;
 
   @override
   void initState() {
@@ -24,7 +25,8 @@ class ExampleState extends State<ExampleWidget> {
   }
 
   _loadDataSource(String geojson) async {
-    MapDataSource dataSource = await MapDataSource.geoJSON(geojson: geojson, labelKey: 'name');
+    MapDataSource dataSource = await MapDataSource.geoJSON(
+        geojson: geojson, keys: ['code'], labelKey: 'code');
     setState(() {
       _dataSource = dataSource;
     });
@@ -38,15 +40,15 @@ class ExampleState extends State<ExampleWidget> {
         MapLayer(
           dataSource: _dataSource!,
           theme: MapValueTheme(
-          contourColor: Colors.white,
-          labelVisibility: (feature) => true,
-          key: 'code',
-          colors: {
-            11250: Colors.green,
-            11251: Colors.red,
-            11252: Colors.orange,
-            11253: Colors.blue
-          }),
+              contourColor: Colors.white,
+              labelVisibility: (feature) => true,
+              key: 'code',
+              colors: {
+                11250: Colors.green,
+                11170: Colors.red,
+                11150: Colors.orange,
+                11253: Colors.blue
+              }),
         )
       ]);
     } else {
@@ -54,9 +56,136 @@ class ExampleState extends State<ExampleWidget> {
     }
 
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-        home: Scaffold(
-            body: Padding(child: content, padding: EdgeInsets.all(32))));
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            leading: Icon(
+              Icons.menu,
+              color: Colors.lightBlueAccent,
+            ),
+            elevation: 0,
+            title: Text(
+              "corona 현황",
+              style: TextStyle(color: Colors.lightBlueAccent),
+            ),
+            centerTitle: true,
+            actions: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Icon(
+                  Icons.notifications,
+                  color: Colors.lightBlueAccent,
+                ),
+              )
+            ],
+          ),
+          body: Stack(
+            children: [
+              Positioned(
+                top: 5,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0xff195f68)),
+                    child: Text(
+                      '07.24 00L00 기준',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(height: 600, child: content),
+              Positioned(
+                top: 50,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.grey),
+                    child: Text(
+                      '증가율',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 50,
+                right: 20,
+                child: Center(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.grey),
+                    child: Text(
+                      '추가 확진자',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 50,
+                left: 40,
+                child: Center(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.lightBlueAccent),
+                    child: Text(
+                      '총 확진자',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 450,
+                left: 100,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('제일 안전한 지역 : 양천구')
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('제일 위험한 지역 : 강남')
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
