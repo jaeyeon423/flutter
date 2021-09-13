69f16aa7cb828adcc06e598eefd7f1bf06356404
jaeyeon <jaeyeon423@gmail.com>
2021-09-13 20:33:54 +0900

    [corona copy] ui 5

--- corona_copy/lib/src/app.dart
+++ corona_copy/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:corona_copy/src/canvas/arrow_clip_path.dart';
 import 'package:corona_copy/src/controller/covid_statistics_controller.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -126,6 +127,7 @@ class App extends GetView<CovidStatisticsController> {
 
   Widget _covitTrendsChart() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(
           "확진자 추이",
@@ -184,7 +186,22 @@ class App extends GetView<CovidStatisticsController> {
                 child: Padding(
                   padding: const EdgeInsets.all(25),
                   child: Column(
-                    children: [_todayStatistics(), _covitTrendsChart()],
+                    children: [
+                      _todayStatistics(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      _covitTrendsChart(),
+                      SizedBox(height: 20,),
+                      AspectRatio(
+                        aspectRatio: 1.7,
+                        child: Card(
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                          child: const _BarChart(),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -194,4 +211,130 @@ class App extends GetView<CovidStatisticsController> {
       ),
     );
   }
+}
+
+class _BarChart extends StatelessWidget {
+  const _BarChart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barTouchData: barTouchData,
+        titlesData: titlesData,
+        borderData: borderData,
+        barGroups: barGroups,
+        alignment: BarChartAlignment.spaceAround,
+        maxY: 20,
+      ),
+    );
+  }
+
+  BarTouchData get barTouchData => BarTouchData(
+    enabled: false,
+    touchTooltipData: BarTouchTooltipData(
+      tooltipBgColor: Colors.transparent,
+      tooltipPadding: const EdgeInsets.all(0),
+      tooltipMargin: 8,
+      getTooltipItem: (
+          BarChartGroupData group,
+          int groupIndex,
+          BarChartRodData rod,
+          int rodIndex,
+          ) {
+        return BarTooltipItem(
+          rod.y.round().toString(),
+          TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        );
+      },
+    ),
+  );
+
+  FlTitlesData get titlesData => FlTitlesData(
+    show: true,
+    bottomTitles: SideTitles(
+      showTitles: true,
+      getTextStyles: (context, value) => const TextStyle(
+        color: Color(0xff7589a2),
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
+      ),
+      margin: 20,
+      getTitles: (double value) {
+        switch (value.toInt()) {
+          case 0:
+            return 'Mn';
+          case 1:
+            return 'Te';
+          case 2:
+            return 'Wd';
+          case 3:
+            return 'Tu';
+          case 4:
+            return 'Fr';
+          case 5:
+            return 'St';
+          case 6:
+            return 'Sn';
+          default:
+            return '';
+        }
+      },
+    ),
+    leftTitles: SideTitles(showTitles: false),
+    topTitles: SideTitles(showTitles: false),
+    rightTitles: SideTitles(showTitles: false),
+  );
+
+  FlBorderData get borderData => FlBorderData(
+    show: false,
+  );
+
+  List<BarChartGroupData> get barGroups => [
+    BarChartGroupData(
+      x: 0,
+      barRods: [
+        BarChartRodData(y: 8, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 1,
+      barRods: [
+        BarChartRodData(y: 10, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 2,
+      barRods: [
+        BarChartRodData(y: 14, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 3,
+      barRods: [
+        BarChartRodData(y: 15, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 3,
+      barRods: [
+        BarChartRodData(y: 13, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 3,
+      barRods: [
+        BarChartRodData(y: 10, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+  ];
 }
