f167324ce863702eae95faed02d1f6074fa3d8b7
jaeyeon <jaeyeon423@gmail.com>
2021-09-14 21:40:37 +0900

    [corona copy] tired

--- corona_copy/lib/src/repository/covid_statistics_repository.dart
+++ corona_copy/lib/src/repository/covid_statistics_repository.dart
@@ -17,15 +17,18 @@ class CovidStatisticsRepository {
     );
   }
 
-  Future<Covid19StatisticsModel> fetchCovid19Statistics() async {
-    print("jaeyyeon");
+  Future<List<Covid19StatisticsModel>> fetchCovid19Statistics(String? startDate, String? endDate) async {
+    var query = Map<String, String>();
+    if(startDate != null)
+      query.putIfAbsent('startCreatDt', () => startDate);
     var response =
-        await _dio.get('/openapi/service/rest/Covid19/getCovid19InfStateJson');
+        await _dio.get('/openapi/service/rest/Covid19/getCovid19InfStateJson', queryParamters: {});
     final document = XmlDocument.parse(response.data);
     final items = document.findAllElements('item');
     print(items.isEmpty);
     if(!items.isEmpty) {
       return Covid19StatisticsModel.fromXml(items.first);
+
     }else{
       return Future.value(null);
     }
