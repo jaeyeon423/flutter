da2ba38a69bcd11c3922b6120758a5f6f6e43206
jaeyeon <jaeyeon423@gmail.com>
2021-09-23 23:28:00 +0900

    [Yotube copy] detail page ui

--- youtube_copy/lib/src/components/yotube_detail.dart
+++ youtube_copy/lib/src/components/yotube_detail.dart
@@ -1,12 +1,145 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:youtube_copy/src/app.dart';
 
 class YoutubeDetail extends StatelessWidget {
+  Widget _titleZone() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "개발하는 남자 유튜브 영상 다시보기",
+            style: TextStyle(fontSize: 15),
+          ),
+          Row(
+            children: [
+              Text(
+                "조회수 1000회",
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              Text(" · "),
+              Text(
+                "2021-09-23",
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _descriptionZone() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+      child: Text(
+        "안녕하세요 개발하는 남자 개남입니다.",
+        style: TextStyle(fontSize: 15),
+      ),
+    );
+  }
+
+  Widget _buttonOne(String iconPath, String text) {
+    return Column(
+      children: [
+        SvgPicture.asset('assets/svg/icons/$iconPath.svg'),
+        Text(text),
+      ],
+    );
+  }
+
+  Widget get line => Container(
+        height: 1,
+        color: Colors.black.withOpacity(0.1),
+      );
+
+  Widget _buttonZone() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buttonOne("like", "1000"),
+        _buttonOne("dislike", "0"),
+        _buttonOne("share", "공유"),
+        _buttonOne("save", "저장"),
+      ],
+    );
+  }
+
+  Widget _ownerZone() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.grey,
+            backgroundImage: Image.network(
+                    "https://yt3.ggpht.com/ytc/AKedOLQagIEl2WOUacXZ8WlCPvApoIouP9sNGkMIDVdQ=s48-c-k-c0x00ffffff-no-rj")
+                .image,
+            radius: 30,
+          ),
+          SizedBox(width: 15,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "개발하는남자",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "구독자 10000",
+                  style: TextStyle(fontSize: 14, color: Colors.black.withOpacity(0.6)),
+                )
+              ],
+            ),
+          ),
+          GestureDetector(child: Text("구독", style: TextStyle(color: Colors.red, fontSize: 16,),),)
+        ],
+      ),
+    );
+  }
+
+  Widget _description() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _titleZone(),
+          line,
+          _descriptionZone(),
+          _buttonZone(),
+          SizedBox(
+            height: 20,
+          ),
+          line,
+          _ownerZone(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(Get.parameters["videoId"] ?? ""),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            height: 250,
+            color: Colors.grey.withOpacity(0.5),
+          ),
+          Expanded(child: _description()),
+        ],
       ),
     );
   }
