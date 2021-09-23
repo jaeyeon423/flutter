d126950cdd888a3ee2663c95066e30bda44158b6
jaeyeon <jaeyeon423@gmail.com>
2021-09-23 18:25:10 +0900

    [Yotube copy] custom appbar

--- youtube_copy/lib/src/pages/home.dart
+++ youtube_copy/lib/src/pages/home.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_copy/src/components/custom_appbar.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text("home"),),
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: CustomAppBar(),
+            floating: true,
+            snap: true,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 150,
+                    color: Colors.grey,
+                  ),
+                );
+              },
+              childCount: 10,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
