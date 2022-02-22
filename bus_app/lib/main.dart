fdcf0c48fb778a9949842c1c52b1ef1c634ea424
jaeyeon <jaeyeon423@gmail.com>
2022-02-22 22:43:35 +0900

    bus app start

---
+++ bus_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'hp bus app',
+      home: Home(),
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "통근 버스"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "셔틀 버스"),
+        ],
+      ),
+    );
+  }
+}
