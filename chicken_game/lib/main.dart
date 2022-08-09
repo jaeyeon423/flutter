2f8ae1e8d33023d298a69d7205cf29d8f1b2dfdd
jaeyeonkim <jaeyeon423@gmail.com>
2022-08-09 22:05:19 +0900

    flame 5

--- chicken_game/lib/main.dart
+++ chicken_game/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/image_composition.dart';
+import 'package:flame/palette.dart';
 import 'package:flutter/material.dart' hide Image;
 
 void main() {
@@ -8,15 +9,21 @@ void main() {
   runApp(GameWidget(game: ChickenGame()));
 }
 
-class ChickenGame extends FlameGame {
+class ChickenGame extends FlameGame with HasDraggables {
   double chickenScaleFactor = 3.0;
 
-  late SpriteComponent chickenSprite;
   late SpriteAnimationComponent chicken;
+  late final JoystickComponent joystick;
+  bool chickenFlipped = false;
+  late SpriteComponent background;
   @override
   Future<void> onLoad() async {
     super.onLoad();
     print('load the assets for the game');
+    background = SpriteComponent()
+      ..sprite = await loadSprite('back.png')
+      ..size = size;
+    add(background);
     Image chickenImage = await images.load('chicken.png');
     var chickenAnimation = SpriteAnimation.fromFrameData(
       chickenImage,
@@ -31,12 +38,30 @@ class ChickenGame extends FlameGame {
       ..size = Vector2(32, 34) * chickenScaleFactor
       ..position = Vector2.all(100);
     add(chicken);
+
+    final knobPaint = BasicPalette.blue.withAlpha(200).paint();
+    final backgroundPaint = BasicPalette.blue.withAlpha(100).paint();
+
+    joystick = JoystickComponent(
+      knob: CircleComponent(radius: 30, paint: knobPaint),
+      background: CircleComponent(radius: 50, paint: backgroundPaint),
+      margin: const EdgeInsets.only(left: 40, bottom: 40),
+    );
+    add(joystick);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    chicken.y += 1;
-    chicken.x += 1;
+    chicken.position.add(joystick.relativeDelta * 300 * dt);
+    if (joystick.relativeDelta[0] < 0 && chickenFlipped) {
+      chickenFlipped = false;
+      chicken.flipHorizontallyAroundCenter();
+    }
+
+    if (joystick.relativeDelta[0] > 0 && !chickenFlipped) {
+      chickenFlipped = true;
+      chicken.flipHorizontallyAroundCenter();
+    }
   }
 }
