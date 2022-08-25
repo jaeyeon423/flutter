a9854078b5b2ea344ff94393812291d6ebfd16c0
jaeyeonkim <jaeyeon423@gmail.com>
2022-08-25 21:43:40 +0900

    devide

--- hair/lib/view/home_page.dart
+++ hair/lib/view/home_page.dart
@@ -14,34 +14,7 @@ class HomePage extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            Container(
-              height: 150,
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 12,
-                itemBuilder: ((context, index) {
-                  return GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      width: 80,
-                      margin: const EdgeInsets.only(left: 20, right: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: index == 11 ? Colors.blueGrey : Colors.black12,
-                      ),
-                      child: HourlyDetails(
-                        index: index,
-                        cardIndex: 1,
-                        temp: 1,
-                        timestamp: 1,
-                        weatherIcon: "sun",
-                      ),
-                    ),
-                  );
-                }),
-              ),
-            ),
+            day_list(),
             Container(
               height: 370,
               color: Colors.red,
--- hair/lib/widgets/hour_widget.dart
+++ hair/lib/widgets/hour_widget.dart
@@ -1,5 +1,43 @@
 import 'package:flutter/material.dart';
 
+class day_list extends StatelessWidget {
+  const day_list({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 12,
+        itemBuilder: ((context, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: 80,
+              margin: const EdgeInsets.only(left: 20, right: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: index == 11 ? Colors.blueGrey : Colors.black12,
+              ),
+              child: HourlyDetails(
+                index: index,
+                cardIndex: 1,
+                temp: 1,
+                timestamp: 1,
+                weatherIcon: "sun",
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
 class HourlyDetails extends StatelessWidget {
   int temp;
   int timestamp;
