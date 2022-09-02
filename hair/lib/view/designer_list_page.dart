9995876e6fef6b3068eb39614c79bb37a5662623
jaeyeonkim <jaeyeon423@gmail.com>
2022-09-02 23:07:41 +0900

    designer list

--- hair/lib/view/designer_list_page.dart
+++ hair/lib/view/designer_list_page.dart
@@ -7,24 +7,71 @@ class DesignerList extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "designer list",
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
       body: Container(
-        decoration: BoxDecoration(color: Colors.deepPurple),
+        // decoration: BoxDecoration(color: Colors.deepPurple),
         width: double.infinity,
         height: size.height,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "DesignerList",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              height: 180,
+              // color: Colors.red,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image(
+                        image: AssetImage(
+                          'assets/images/0.jpg',
+                        ),
+                        width: 80,
+                      ),
+                      Image(
+                        image: AssetImage(
+                          'assets/images/0.jpg',
+                        ),
+                        width: 80,
+                      ),
+                      Image(
+                        image: AssetImage(
+                          'assets/images/0.jpg',
+                        ),
+                        width: 80,
+                      ),
+                      Image(
+                        image: AssetImage(
+                          'assets/images/0.jpg',
+                        ),
+                        width: 80,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "부원장 소희 - 12년",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text("아트라 광교점"),
+                  Text("4.9(2194) |  10:00 ~ 21:00")
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
