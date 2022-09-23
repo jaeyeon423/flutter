47cac489dabb822319390d82c6fb57078416c1a0
jaeyeonkim <jaeyeon423@gmail.com>
2022-09-23 19:07:38 +0900

    webview ex

---
+++ webview/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'dart:io';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: WebView(
+        initialUrl:
+            'https://m.place.naver.com/hairshop/13502147/stylist/3782681?entry=plt',
+        javascriptMode: JavascriptMode.unrestricted,
+      )),
+    );
+  }
+}
