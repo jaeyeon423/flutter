458059364f2be563c312a67ff080b657ba1bd4f5
jaeyeon <jaeyeon423@gmail.com>
2024-01-02 21:15:41 +0900

    Global Key

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  @override
   Widget build(BuildContext context) {
+
+    void _addItem(){
+      Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => NewItem()));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(onPressed: _addItem, icon: Icon(Icons.add)),
+        ],
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) => ListTile(
