aad2ad193d75bf6ef8e4b856b6e86e51a7ce0267
jaeyeon <jaeyeon423@gmail.com>
2024-01-13 22:03:36 +0900

    save chat text

--- chat_firebase/lib/widgets/new_message.dart
+++ chat_firebase/lib/widgets/new_message.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 
 class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
@@ -16,17 +19,29 @@ class _NewMessageState extends State<NewMessage> {
     super.dispose();
   }
 
-  void _submitMessage(){
+  void _submitMessage() async{
     final enteredMessage = _messageController.text;
 
     if(enteredMessage.trim().isEmpty){
       return;
     }
 
-    // send to Firebase
-
-
+    FocusScope.of(context).unfocus();
     _messageController.clear();
+
+
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+
+    // send to Firebase
+    FirebaseFirestore.instance.collection('chat').add({
+      'text':enteredMessage,
+      'createdAt':Timestamp.now(),
+      'userId': user.uid,
+      'username':userData.data()!['username'],
+      'userImage':userData.data()!['image_url'],
+    });
+
   }
 
   @override
