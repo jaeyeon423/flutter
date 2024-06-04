2897db88e892dd4175b505eadafb05255fdc3e47
jaeyeon <jaeyeon423@gmail.com>
2024-06-04 21:58:17 +0900

    avoid unnecessary request

--- ex/lib/widgets/grocery_list.dart
+++ ex/lib/widgets/grocery_list.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
+import 'package:ex/data/categories.dart';
 import 'package:ex/data/dummy_items.dart';
 import 'package:ex/models/grocery_item.dart';
 import 'package:ex/widgets/new_item.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,7 +16,38 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-baf78-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+            (element) => element.value.title == item.value['category'],
+          )
+          .value;
+      loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category));
+    }
+    setState(() {
+      _groceryItems = loadedItems;
+    });
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
--- ex/lib/widgets/new_item.dart
+++ ex/lib/widgets/new_item.dart
@@ -20,22 +20,32 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async{
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
           'flutter-prep-baf78-default-rtdb.asia-southeast1.firebasedatabase.app',
           'shopping-list.json');
-      http.post(url,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: json.encode({
+      final response = await  http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
             'name': _enteredName,
             'quantity': _enteredQuantity,
             'category': _selectedCategory.title,
-          }));
-      Navigator.of(context).pop();
+          },
+        ),
+      );
+
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      if (!context.mounted)
+        return;
+
+      Navigator.of(context).pop(GroceryItem(id: resData['name'], name: _enteredName, quantity: _enteredQuantity, category: _selectedCategory));
     }
   }
 
