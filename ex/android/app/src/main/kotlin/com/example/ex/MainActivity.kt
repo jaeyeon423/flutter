be0a258a1e239c09cfce50d02f614f04533b18b4
jaeyeon <jaeyeon423@gmail.com>
2024-11-20 21:41:35 +0900

    remove previous projects

--- ex/android/app/src/main/kotlin/com/example/ex/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- riverpod_ex1/android/app/src/main/kotlin/com/example/riverpod_ex1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.riverpod_ex1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
