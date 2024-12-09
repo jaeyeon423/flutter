376438b1b23b544a8599821e742dda50c0cafdc9
jaeyeon <jaeyeon423@gmail.com>
2024-12-09 23:39:31 +0900

    enum_activity_provider.dart

---
+++ notifier_provider/lib/pages/enum_activity/enum_activity_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'enum_activity_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$enumActivityHash() => r'd36b8c99d7f611706efaf33070bdc6de20c11061';
+
+/// See also [EnumActivity].
+@ProviderFor(EnumActivity)
+final enumActivityProvider =
+    AutoDisposeNotifierProvider<EnumActivity, EnumActivityState>.internal(
+  EnumActivity.new,
+  name: r'enumActivityProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$enumActivityHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EnumActivity = AutoDisposeNotifier<EnumActivityState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
