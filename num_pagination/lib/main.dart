7e3def5dd7493277b0679dca1d7df36845c24ce8
jaeyeon <jaeyeon423@gmail.com>
2025-01-02 23:06:32 +0900

    start pagina

--- num_pagination/lib/main.dart
+++ num_pagination/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Numbered Pagination',
+      title: 'Numbered Pagination ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
