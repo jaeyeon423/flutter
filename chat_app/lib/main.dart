08271349de184fa1684464608b295cbb37efa083
jaeyeon <jaeyeon423@gmail.com>
2025-05-13 14:07:04 +0900

    auth page

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class App extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      // home: ...
+      home: AuthScreen(),
     );
   }
 }
