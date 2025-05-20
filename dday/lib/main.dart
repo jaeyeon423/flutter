942c13e9c62241fb93a26ef2087c3adb2f6f3381
jaeyeon <jaeyeon423@gmail.com>
2025-05-20 14:14:43 +0900

    app deploy

--- dday/lib/main.dart
+++ dday/lib/main.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'screens/dday_management_screen.dart';
 
 void main() {
+  // Initialize FFI for desktop platforms
+  if (Platform.isWindows || Platform.isLinux) {
+    // Initialize FFI
+    sqfliteFfiInit();
+    // Change the default factory
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
